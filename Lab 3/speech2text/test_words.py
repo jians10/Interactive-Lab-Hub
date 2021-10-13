156e07b8360c6db581565e700d2bcff680d24c01
unknown <sj544@cornell.edu>
2021-10-13 16:41:11 -0400

    this

--- Lab 3/speech2text/test_words.py
+++ Lab 3/speech2text/test_words.py
@@ -16,7 +16,7 @@ if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE
 
 model = Model("model")
 # You can also specify the possible word list
-rec = KaldiRecognizer(model, wf.getframerate(), "zero oh one two three four five six seven eight nine [unk]")
+rec = KaldiRecognizer(model, wf.getframerate(), "zero one two three four five six seven eight nine [unk]")
 
 while True:
     data = wf.readframes(4000)
@@ -28,6 +28,4 @@ while True:
        rec.PartialResult()
 
 print(rec.FinalResult())
-os.system('./parrot.sh ')
-os.system(rec.FinalResult())
 
