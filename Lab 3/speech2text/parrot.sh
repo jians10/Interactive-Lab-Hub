4d1c39b7422d9f1162cc7e1b6d5eef1d5082f4fe
unknown <sj544@cornell.edu>
2021-10-13 16:58:30 -0400

    this

--- Lab 3/speech2text/parrot.sh
+++ Lab 3/speech2text/parrot.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 # from https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)
-espeak -ven+f2 -k5 -s150 --stdout  "your phone number is $1 $2 $3" | aplay
+espeak -ven+f2 -k5 -s150 --stdout  "your phone number is $1 $2 $3 $4 $5 $6 $7" | aplay
